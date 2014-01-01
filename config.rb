2d100f79866c82a3adedf1afee6e9e5fff875b61
Mathew Miquel <brandonmiquel@gmail.com>
2013-12-31 23:10:17 -0500

    Remove sqlite

--- config.rb
+++ config.rb
@@ -1,4 +1,3 @@
 
-INPUT_DIR = '/Sweet Dee/Photos_to_sort/'
-OUTPUT_DIR = '/Sweet Dee/Photo_output/'
-DB_PATH = 'db/photos.sqlite3'
+INPUT_DIR = '/Volumes/Sweet Dee/Photos_to_sort/'
+OUTPUT_DIR = '/Volumes/Sweet Dee/Photo_output/'
